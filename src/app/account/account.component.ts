74fd0bb1a9e9401db87bcdd0128019ff58e186c0
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-01-13 22:34:57 +0530

    Create Services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -15,6 +15,5 @@ export class AccountComponent{
   onSetTo(status: string)
   {
     this.statusChanged.emit({id:this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
   }
 }
